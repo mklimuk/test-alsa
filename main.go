976f90057f70216e902fdcbc39463827a32562a4
Michal Klimuk <michal.klimuk@neo9.pl>
2016-12-05 20:41:26 +0100

    fix compilation error

--- main.go
+++ main.go
@@ -24,18 +24,17 @@ func main() {
 	conf := &config.AudioConf{DeviceBuffer: 4096, ReadBuffer: 8192, PeriodFrames: 2048, Periods: 2}
 	params := &audio.BufferParams{BufferFrames: conf.DeviceBuffer, PeriodFrames: conf.PeriodFrames, Periods: conf.Periods}
 
-
 	var err error
 	var dev audio.PlaybackDevice
 	if dev, err = alsa.NewPlaybackDevice(params); err != nil {
 		panic(err)
 	}
 
-	p := audio.New(conf, , "/etc/husar/dong.wav")
+	p := audio.New(conf, dev, "/etc/husar/dong.wav")
 	f := websocket.NewFactory()
 
 	clog.Info("Initializing REST router...")
-	if z := api.NewPlaybackAPI(p, f)
+	z := api.NewPlaybackAPI(p, f)
 
 	router := gin.New()
 	z.AddRoutes(router)
