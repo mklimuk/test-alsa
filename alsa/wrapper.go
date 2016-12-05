c3462b57766a63c08464eb4547bcc7e49c82754d
Michal Klimuk <michal.klimuk@neo9.pl>
2016-12-05 20:35:36 +0100

    fixed alsa compilation errors

--- alsa/wrapper.go
+++ alsa/wrapper.go
@@ -8,9 +8,9 @@ import (
 const channels = 1
 const rate = 22050
 const deviceName = "sysdefault"
-const format = alsa.FormatS16LE
+const format = goalsa.FormatS16LE
 
 //NewPlaybackDevice wraps goalsa PlaybackDevice constructor for testing convienience
-func NewPlaybackDevice(bp *audio.BufferParams) (p *audio.PlaybackDevice, err error) {
+func NewPlaybackDevice(bp *audio.BufferParams) (p audio.PlaybackDevice, err error) {
 	return goalsa.NewPlaybackDevice(deviceName, channels, format, rate, goalsa.BufferParams{BufferFrames: bp.BufferFrames, PeriodFrames: bp.PeriodFrames, Periods: bp.Periods})
 }
