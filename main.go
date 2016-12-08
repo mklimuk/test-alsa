e49f78c8dca2696b86b810bfd2230ae88efc0fc0
Michal Klimuk <michal.klimuk@neo9.pl>
2016-12-08 17:44:24 +0100

    feat(logging): configurable logging

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 
 	log "github.com/Sirupsen/logrus"
@@ -19,12 +20,23 @@ const (
 func main() {
 	clog := log.WithFields(log.Fields{"logger": "mic-receiver.main"})
 
-	log.SetLevel(log.InfoLevel)
+	//read config path and log level from runtime flags
+	//var configPath string
+	//flag.StringVar(&configPath, "config", defaultConfigPath, "Path to yaml configuration file")
+	level := flag.String("log", "info", "Log level")
+	flag.Parse()
+
+	var l log.Level
+	var err error
+	if l, err = log.ParseLevel(*level); err != nil {
+		clog.WithField("level", level).Warn("Invalid log level. Fallback to info.")
+		l = log.InfoLevel
+	}
+	log.SetLevel(l)
 
 	conf := &config.AudioConf{DeviceBuffer: 4096, ReadBuffer: 8192, PeriodFrames: 2048, Periods: 2}
 	params := &audio.BufferParams{BufferFrames: conf.DeviceBuffer, PeriodFrames: conf.PeriodFrames, Periods: conf.Periods}
 
-	var err error
 	var dev audio.PlaybackDevice
 	if dev, err = alsa.NewPlaybackDevice(params); err != nil {
 		panic(err)
